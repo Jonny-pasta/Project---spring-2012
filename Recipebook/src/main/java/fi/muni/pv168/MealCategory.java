9928e1f5b19774179f125214bd3c628b9a23888e
Milos Lukacka <mimo@tulak.sk>
2013-03-05 14:44:37 +0100

    5.3.2013
    added enum MealCategory and enum MealType

---
+++ Recipebook/src/main/java/fi/muni/pv168/MealCategory.java
@@ -0,0 +1,9 @@
+package fi.muni.pv168;
+
+/**
+ * enumeration of meal category, what's the key element in the meal
+ * @author Mimo
+ */
+public enum MealCategory {
+    MEAT, MEATLESS, FISH, SWEET, ALCOHOLIC, NONALCOHOLIC, PASTA
+}
---
+++ Recipebook/src/main/java/fi/muni/pv168/MealType.java
@@ -0,0 +1,9 @@
+package fi.muni.pv168;
+
+/**
+ * enumeration of meal types, when and how should be the meal served
+ * @author Mimo
+ */
+public enum MealType {
+    BREAKFAST, APPETIZER, SOUP, MAIN_DISH, DESSERT, SALADS, DRINKS
+}
