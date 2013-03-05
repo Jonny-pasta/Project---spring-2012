d2da9e85341da6f3e9044205224208cbca221812
Milos Lukacka <mimo@tulak.sk>
2013-03-05 13:57:26 +0100

    1st try
    -nothing-

---
+++ Recipebook/src/main/java/fi/muni/pv168/App.java
@@ -0,0 +1,13 @@
+package fi.muni.pv168;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
